3ddd4141d2bc82e2aa31b70851b8d4fc65a6d5e6
Izan Estirado Alfaro <40562916+AstroIzan@users.noreply.github.com>
2026-08-29 01:19:40 +0200

    feat(hobbie): update stetics about hobbie section

--- client/src/app/features/global/home/home.component.ts
+++ client/src/app/features/global/home/home.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.work.scss', './home.hobbie.scss']
 })
 export class HomeComponent {
   @Input() siteMode: 'work' | 'hobbie' = 'work';
